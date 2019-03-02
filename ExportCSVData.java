eff15f4b1d8be1e27e615b77586a0e3399bf53bd
Boram Jung <bbo0026@gmail.com>
2019-03-02 18:30:14 +0900

    completed

--- ExportCSVData.java
+++ ExportCSVData.java
@@ -19,23 +19,63 @@ public class ExportCSVData {
             String getCountry = record.get("Country");
             //check if it contains country info
             if(getCountry.contains(country)){
-                System.out.println(getCountry + " : " 
-                +record.get("Exports")+" : "+record.get("Value (dollars)"));
+                System.out.println(getCountry + ": " 
+                +record.get("Exports")+": "+record.get("Value (dollars)"));
                 found = true;
             } 
-        }if (!found) System.out.println("NOT FOUND");
+        }
+        if(!found) System.out.println("NOT FOUND");
     }
     
+    //print exporting 2 items
+    public void listExportersTwoProducts(CSVParser parser, String exportItem1, String exportItem2){
+            for(CSVRecord record : parser){
+            //look at the "Exports" column
+            String export = record.get("Exports");
+            if(export.contains(exportItem1) && export.contains(exportItem2)){
+                System.out.println(record.get("Country"));
+            }
+        }
+    }
     
+    //print the number of countries that export certain item
+    public void numberOfExporters(CSVParser parser, String exportItem){
+        int num = 0;    
+        for(CSVRecord record : parser){
+            //look at the "Exports" column
+            String export = record.get("Exports");
+            if(export.contains(exportItem)) {
+               num++;
+              }
+	}
+	System.out.println("How many countries export " + exportItem + ": " + num);
+    }
     
-    
-    
+    //print countries exporting total amount over $999,999,999,999
+    public void bigExporters(CSVParser parser, String amount){
+        String totalAmount = "$999,999,999";   
+        for(CSVRecord record : parser){
+            //look at the "Value (dollars)" column
+            String value = record.get("Value (dollars)");
+            if(value.length() > totalAmount.length()) {
+               System.out.println(record.get("Country")+" "+value);
+              }
+	}
+	
+    }
     
     //tester
     public void tester(){
         FileResource fr = new FileResource();
         CSVParser parser = fr.getCSVParser();
         countryInfo(parser,"Germany");
+        parser = fr.getCSVParser();
+        listExportersTwoProducts(parser, "gold", "diamonds");
+        parser = fr.getCSVParser();
+        numberOfExporters(parser, "gold");
+        parser = fr.getCSVParser();
+        bigExporters(parser, "$999,999,999");
+        parser = fr.getCSVParser();
     }
 }
 
